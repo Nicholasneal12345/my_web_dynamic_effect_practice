007e598a2ce7cb57518f70daea0fd9a5526ac11a
Nicholasneal12345 <410203041@gms.ndhu.edu.tw>
2021-11-24 21:25:40 +0800

    瀏覽器下拉，全螢幕畫面縮小成header，並且鎖死(只能使用一次，除非重新載入此頁面)

---
+++ js/FullscreenAnimatedStickyHeader.js
@@ -0,0 +1,22 @@
+    const header = document.querySelector("div.header");
+
+    const dropDown = document.querySelector("div.dropDown");
+
+    const homeFullImage = document.querySelector("div.header > img.homeFullImage");
+
+
+
+
+    window.addEventListener("scroll", function(){
+        
+        header.classList.add("sticky", window.scrollY > 0);
+
+        homeFullImage.setAttribute("src", "../image/FullscreenAnimatedStickyHeader/plants-gf423cd171_1280.jpg");
+
+        window.scrollTo(0, 0); //鎖住畫面，讓他做到一次性滑動
+    });
+
+    dropDown.addEventListener("click", function(){
+        
+        header.classList.toggle("sticky");
+    })
